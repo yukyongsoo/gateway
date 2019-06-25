f4c001cf1c661f560a020b075e447dad6ca0ea40
yongsoo <yozx9079@naver.com>
2019-06-26 00:38:37 +0900

    route for auth check

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/yuk/msa/gateway/GatewayApplication.kt
+++ src/main/kotlin/yuk/msa/gateway/GatewayApplication.kt
@@ -2,10 +2,12 @@ package yuk.msa.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 class GatewayApplication
 
 fun main(args: Array<String>) {
